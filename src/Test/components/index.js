d7ab96d529024ebf9a40edfd0c7abc2a9a04d867
O-Seonsik <dhtjstlr777@gmail.com>
2020-02-02 15:48:14 +0900

    Changed Warning directory name for push

--- src/Test/components/index.js
+++ src/Test/components/index.js
@@ -4,7 +4,7 @@ import Navigate from "./Navigate/Navigate";
 import Post from './Post/Post';
 import CommentList from './CommentList/CommentList';
 import Comment from './Comment/Comment';
-import Warning from "./  Warning/Warning";
+import Warning from "./Warning/Warning";
 
 export{
     Header,
