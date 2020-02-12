1833e785eac07529afd768fe9f8b3186f9c1a14c
O-Seonsik <dhtjstlr777@gmail.com>
2020-02-13 03:20:47 +0900

    Add Post alertMap component

---
+++ src/components/MapAlert.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import '../style/MapAlert.scss'
+import Maps from "./Maps";
+
+class MapAlert extends Component{
+    state ={
+        onDisplay: true,
+    }
+    clicked = () => {
+        this.setState({
+            onDisplay: false,
+        })
+        setTimeout(()=>{
+            this.props.clicked(false);
+        }, 300);
+    }
+    render() {
+        return (
+            <div>
+                {this.state.onDisplay ?
+                    <div className="alert animated fadeIn" onClick={this.clicked}></div>
+                    : <div className="alert animated fadeOut" onClick={this.clicked}></div>}
+                <Maps clicked={this.state.onDisplay} />
+            </div>
+        );
+    }
+}
+
+
+export default MapAlert;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,39 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Post from 'components/Post'
+import MapAlert from "../components/MapAlert";
+import '../Animation.css'
 
-const Home = () => {
-    return (
-        <div>
-            <Post />
-            <hr/>
-        </div>
-    )
-};
+class Home extends Component{
+    state = {
+        clicked: false,
+    }
+
+    checkClicked = (getClicked) => {
+        this.setState({
+            clicked: getClicked,
+        })
+    }
+    render(){
+        return(
+            <div>
+                {this.state.clicked && <MapAlert clicked = {this.checkClicked}/>}
+                <Post clicked = {this.checkClicked}/>
+                <Post clicked = {this.checkClicked}/>
+                <Post clicked = {this.checkClicked}/>
+                <Post clicked = {this.checkClicked}/>
+                <Post clicked = {this.checkClicked}/>
+                <Post clicked = {this.checkClicked}/>
+                <Post clicked = {this.checkClicked}/>
+                <Post clicked = {this.checkClicked}/>
+                <Post clicked = {this.checkClicked}/>
+                <Post clicked = {this.checkClicked}/>
+                <Post clicked = {this.checkClicked}/>
+                <Post clicked = {this.checkClicked}/>
+                <Post clicked = {this.checkClicked}/>
+                <Post clicked = {this.checkClicked}/>
+            </div>
+        );
+    }
+}
 
 export default Home;
