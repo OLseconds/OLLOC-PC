d3ef0d86010d389544fd28b6fc585b2411841f21
O-Seonsik <dhtjstlr777@gmail.com>
2020-02-14 02:38:08 +0900

    Add MyPost Component and MyPost Directory

---
+++ src/components/MyPost/MyTimeline.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const test = () => {
+    alert("!");
+}
+
+const MyTimeline  = () => {
+    // test();
+    return(
+        <div id = "my-timeline">
+            test
+        </div>
+    );
+}
+
+export default MyTimeline;
---
+++ src/pages/MyPost.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import MyTimeline from "../components/MyPost/MyTimeline";
+
+const MyPost = () => {
+    return(
+        <MyTimeline />
+    )
+}
+
+export default MyTimeline;
