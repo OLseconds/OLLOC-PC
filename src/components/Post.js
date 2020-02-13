bb4f6c7d05baa839a6840cdf293658217e2884b6
O-Seonsik <dhtjstlr777@gmail.com>
2020-02-13 10:12:21 +0900

    Preparing to receive a RESTful API on the Profile Image

--- src/components/Post.js
+++ src/components/Post.js
@@ -15,6 +15,7 @@ class Post extends Component{
             "나도 춤추고 싶은데\n" +
             "2만원도 있는데\n" +
             "씨발 나만 못 가",
+        profileImg: "https://scontent-gmp1-1.cdninstagram.com/v/t51.2885-19/s320x320/80639092_738375116652935_9072838350867529728_n.jpg?_nc_ht=scontent-gmp1-1.cdninstagram.com&amp;_nc_ohc=GnlK_dazdqwAX8A28P1&amp;oh=ddf92afe97259dea34548c1b70a9521b&amp;oe=5F003D5B",
         imagesURL: [
             "https://placehold.it/458x458",
             "https://placehold.it/128x128",
@@ -47,10 +48,10 @@ class Post extends Component{
     }
 
     render(){
-        const {description, likes, likeState, information, imagesURL} = this.state;
+        const {description, likes, likeState, information, imagesURL, profileImg} = this.state;
         return (
             <div id = "post">
-                <div id = "writer"><img src="https://placehold.it/48x48" /> paperlee_</div>
+                <div id = "writer"><img src={profileImg} /> paperlee_</div>
                 <PostImages URL={imagesURL} clicked={this.props.clicked}/>
                 <PostInfo
                     initDescription={description}
